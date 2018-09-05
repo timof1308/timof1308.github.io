fe316be7a34e89f5c529673cc08af11119daf043
timofischer <timo.fischer@eu.didata.com>
2018-09-05 19:41:22 +0200

    * mod index.html + added validator sample

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>timof1996</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge;"/>
+
+    <link href="https://unpkg.com/cirrus-ui" type="text/css" rel="stylesheet"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .pageWrapper {
+            padding-top: 60px;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="header header-fixed unselectable header-animated">
+    <div class="header-brand">
+        <div class="nav-item no-hover">
+            <h6 class="title">timof1996</h6>
+        </div>
+        <div class="nav-item nav-btn" id="header-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="header-nav" id="header-menu">
+        <div class="nav-right">
+            <div class="nav-item text-center">
+                <a href="https://github.com/timof1996" target="_blank">
+                    <span class="icon">
+                        <i class="fa fa-github" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+            <div class="nav-item text-center">
+                <a href="https://twitter.com/TimoF_1308" target="_blank">
+                    <span class="icon">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+            <div class="nav-item text-center">
+                <a href="https://www.linkedin.com/in/timo-fischer-07a587169/" target="_blank">
+                    <span class="icon">
+                        <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="pageWrapper">
+    <div class="content">
+        <div class="row">
+            <h2 class="title">timof1996</h2>
+            <h6 class="light subtitle">Webpage powered by <b>github.io</b></h6>
+            <div class="divider">
+        </div>
+
+        <div class="row">
+            <div class="col-fluid">
+                <div class="card">
+                    <div class="card-container">
+                        <div class="card-image"
+                             style="background-image: url(https://images.unsplash.com/photo-1536008046477-01746710ffb9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=10873349a7093bce26753c49f22be3e8)"></div>
+                        <!--https://source.unsplash.com/random-->
+                        <div class="title-container">
+                            <p class="title">Form Validation</p><span class="subtitle">by Timo F</span>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <p>JavaScript form validation designed for Bootstrap v3.</p>
+                    </div>
+                    <div class="action-bar center">
+                        <a href="https://github.com/timof1996/bootstrapValidation" target="_blank" class="btn">ON GIT</a>
+                        <a href="validator" target="_blank" class="btn">LIVE VIEW</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-fluid"></div>
+        </div>
+
+    </div>
+</div>
+
+<footer>
+    <div class="content">
+        <p class="font-thin center">timof1996 &copy; <?php echo date("Y"); ?>.</p>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script>
+
+</script>
+</body>
+</html>
